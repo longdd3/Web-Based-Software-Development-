c5ae31a9e750b0238435cb0bd9c46c7abfb930f1
Dang Long <43181516+longdd3@users.noreply.github.com>
2020-02-06 06:35:20 -0600

    Change Font and Back Ground Color Buttons

---
+++ changebgcolor.js
@@ -0,0 +1,12 @@
+function changeColor(colorName) {
+    document.body.style.background = colorName;
+  }
+function changefont1()
+{
+ document.getElementById("alltext").style.fontFamily = "sans-serif";
+}
+
+   function changefont2()
+{
+ document.getElementById("alltext").style.fontFamily = "serif";
+}
